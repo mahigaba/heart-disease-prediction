ba904a9b84ff7e79c46a96744c802df532464935
Mahi <mahibfgi@gmail.com>
2026-08-20 11:34:52 +0530

    Update heartdisease.py

--- heartdisease.py
+++ heartdisease.py
@@ -43,7 +43,7 @@ st.markdown("### Powered by XGBoost — Fast, Accurate & Reliable")
 # ----------------------------------------
 # 📊 Load & Prepare Data
 # ----------------------------------------
-df = pd.read_csv(r"C:\Users\PC\OneDrive\Documents\data analyst\heart disease prediction\heart.csv")
+df = pd.read_csv(r"heart.csv")
 X = df.drop("target", axis=1)
 y = df["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
